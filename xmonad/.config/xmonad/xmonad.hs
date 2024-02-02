a962e8b11e47f058a5d8ebe90e875ba64b598cbf
Himanshu Himsurya <himanshuhimsurya@gmail.com>
2024-02-03 01:55:20 +0530

    Add (extra) print screen keybind
        Misc: formatting changes

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -152,66 +152,73 @@ myKeys conf@(XConfig { XMonad.modMask = modm }) =
     , ((modm, xK_p), spawn myRofi)
     , ((modm .|. shiftMask, xK_z), spawn myRofiBluetooth)
     , ((modm .|. shiftMask, xK_p), spawn myRofiBluetooth)
-    ] ++
+    ]
+
+    ++
+
+    -- take (selective) screenshots
+    [ ((modm, xK_s), spawn screenshot)
+    , ((0, xK_Print), spawn screenshot)
+    ]
+
+    ++
+
     -- launch default terminal
-       [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch alacritty
-       , ((modm .|. shiftMask, xK_Tab), spawn "alacritty")
+    , ((modm .|. shiftMask, xK_Tab), spawn "alacritty")
 
     -- Lock Screen
-       , ((modm .|. shiftMask, xK_l), spawn "slock")
-
-    -- take full screenshot
-       , ((modm, xK_s), spawn screenshot)
+    , ((modm .|. shiftMask, xK_l), spawn "slock")
 
     -- close focused window
-       , ((modm .|. shiftMask, xK_c), kill)
+    , ((modm .|. shiftMask, xK_c), kill)
 
      -- Rotate through the available layout algorithms
-       , ((modm, xK_space), sendMessage NextLayout)
+    , ((modm, xK_space), sendMessage NextLayout)
 
     --  Reset the layouts on the current workspace to default
-       , ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
+    , ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
 
     -- Resize viewed windows to the correct size
-       , ((modm, xK_n), refresh)
+    , ((modm, xK_n), refresh)
 
     -- Move focus to the next window
-       , ((modm, xK_Tab), windows W.focusDown)
+    , ((modm, xK_Tab), windows W.focusDown)
 
     -- Move focus to the next window
-       , ((modm, xK_j), windows W.focusDown)
+    , ((modm, xK_j), windows W.focusDown)
 
     -- Move focus to the previous window
-       , ((modm, xK_k), windows W.focusUp)
+    , ((modm, xK_k), windows W.focusUp)
 
     -- Move focus to the master window
-       , ((modm, xK_m), windows W.focusMaster)
+    , ((modm, xK_m), windows W.focusMaster)
 
     -- Swap the focused window and the master window
-       , ((modm, xK_Return), windows W.swapMaster)
+    , ((modm, xK_Return), windows W.swapMaster)
 
     -- Swap the focused window with the next window
-       , ((modm .|. shiftMask, xK_j), windows W.swapDown)
+    , ((modm .|. shiftMask, xK_j), windows W.swapDown)
 
     -- swap the focused window with the previous window
-       , ((modm .|. shiftMask, xK_k), windows W.swapUp)
+    , ((modm .|. shiftMask, xK_k), windows W.swapUp)
 
     -- Shrink the master area
-       , ((modm, xK_h), sendMessage Shrink)
+    , ((modm, xK_h), sendMessage Shrink)
 
     -- Expand the master area
-       , ((modm, xK_l), sendMessage Expand)
+    , ((modm, xK_l), sendMessage Expand)
 
     -- Push window back into tiling
-       , ((modm, xK_t), withFocused $ windows . W.sink)
+    , ((modm, xK_t), withFocused $ windows . W.sink)
 
     -- Increment the number of windows in the master area
-       , ((modm, xK_comma), sendMessage (IncMasterN 1))
+    , ((modm, xK_comma), sendMessage (IncMasterN 1))
 
     -- Decrement the number of windows in the master area
-       , ((modm, xK_period), sendMessage (IncMasterN (-1)))
+    , ((modm, xK_period), sendMessage (IncMasterN (-1)))
 
     -- Toggle the status bar gap
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
@@ -220,38 +227,39 @@ myKeys conf@(XConfig { XMonad.modMask = modm }) =
     , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
-       , ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess))
+    , ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess))
 
     -- Restart xmonad
-       , ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart")
+    , ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart")
 
     -- Run xmessage with a summary of the default keybindings (useful for beginners)
-       , ((modm .|. shiftMask, xK_slash)
-         , spawn ("echo \"" ++ help ++ "\" | xmessage -file -")
-         )
-       ]
+    , ((modm .|. shiftMask, xK_slash)
+        , spawn ("echo \"" ++ help ++ "\" | xmessage -file -")
+      )
+    ]
+
     ++
 
-    --
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
+    [ ((m .|. modm, k), windows $ f i)
+    | (i, k) <- zip (XMonad.workspaces conf) [xK_0 .. xK_9]
+    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    ]
 
-       [ ((m .|. modm, k), windows $ f i)
-       | (i, k) <- zip (XMonad.workspaces conf) [xK_0 .. xK_9]
-       , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-       ]
     ++
 
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
-       [ ((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-       | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
-       , (f  , m ) <- [(W.view, 0), (W.shift, shiftMask)]
-       ]
+    [ ((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
+    , (f  , m ) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
 
     ++
-       [ ((modm, xK_a), nextScreen >> warpToWindow(0.9) (0.2)) ]
+
+    -- Shift focus across monitors/screens
+    [ ((modm, xK_a), nextScreen >> warpToWindow(0.9) (0.2)) ]
 
 
 ------------------------------------------------------------------------
